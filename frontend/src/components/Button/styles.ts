a47d85ab4246037346c51e83794bd5de57f31253
Marcos Mathias <marcos-github@memathias.com>
2020-08-18 17:09:56 -0300

    Create Basic Components

--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -16,22 +16,3 @@ export const Container = styled.button`
     background: ${shade(0.2, '#860209')};
   }
 `;
-
-/**
- *  button {
-    background: #860209;
-    color: #d5d5d5;
-    font-weight: 500;
-    height: 56px;
-    border-radius: 10px;
-    border: 0;
-    padding: 0 16px;
-    width: 100%;
-    margin-top: 16px;
-    transition: background-color 0.2s;
-
-    &:hover {
-      background: ${shade(0.2, '#860209')};
-    }
-  }
- */
