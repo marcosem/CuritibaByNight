b314537bce580669082c45c9133ffa4591f20d10
Marcos Mathias <marcos-github@memathias.com>
2020-08-18 19:53:16 -0300

    Fix CRLF to LF

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 // import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignUp from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
