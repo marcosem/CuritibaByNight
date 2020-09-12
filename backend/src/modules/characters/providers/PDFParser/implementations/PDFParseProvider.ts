0cc6f716bbdcae348c0f49accccfdf4758854afb
Marcos Mathias <marcos-github@memathias.com>
2020-09-12 15:49:44 -0300

    Fix parse mortal character sheet issie

--- backend/src/modules/characters/providers/PDFParser/implementations/PDFParseProvider.ts
+++ backend/src/modules/characters/providers/PDFParser/implementations/PDFParseProvider.ts
@@ -32,12 +32,16 @@ class PDFParseProvider implements IPDFParserProvider {
     let isTitled = true;
     let isParsed = false;
     let clan: string;
+    let isMortal = false;
 
     rl.on('line', line => {
       index += 1;
+
       if (index === 3 && line.indexOf('Curitiba By Night') === -1) {
         isTitled = false;
         char.name = line.substring(0, line.length - 1);
+      } else if (index === 4 && line.indexOf('Mortal') >= 0) {
+        isMortal = true;
       } else if (index === 4 && isTitled) {
         char.name = line.substring(0, line.length - 1);
       } else if (line.indexOf('Experience Unspent: ') >= 0 && !experience) {
@@ -52,18 +56,39 @@ class PDFParseProvider implements IPDFParserProvider {
           char.experience = experience;
           isParsed = true;
         }
-      } else if (line.indexOf('Clan: ') >= 0 && !clan) {
-        const startClan = line.indexOf('Clan: ') + 'Clan: '.length;
-        const endClan = line.indexOf('Generation: ') - 1;
+      } else if (!clan) {
+        if (isMortal) {
+          if (line.indexOf('Affiliation: ') >= 0) {
+            const startClan =
+              line.indexOf('Affiliation: ') + 'Affiliation: '.length;
+            const endClan = line.indexOf('Motivation:') - 1;
 
-        clan = line.substring(startClan, endClan);
-        char.clan = clan;
+            clan = line.substring(startClan, endClan);
+            char.clan = clan;
 
-        rl.close();
+            rl.close();
+          }
+        } else if (line.indexOf('Clan: ') >= 0) {
+          const startClan = line.indexOf('Clan: ') + 'Clan: '.length;
+          const endClan = line.indexOf('Generation:') - 1;
+
+          clan = line.substring(startClan, endClan);
+          char.clan = clan;
+
+          rl.close();
+        }
       }
     });
 
     await once(rl, 'close');
+
+    if (isMortal) {
+      const newName = char.name.split(' - ');
+      if (newName.length > 1) {
+        // eslint-disable-next-line prefer-destructuring
+        char.name = newName[1];
+      }
+    }
 
     if (!isParsed) {
       return undefined;
