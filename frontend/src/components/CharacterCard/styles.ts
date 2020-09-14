2d60bea6aaf82fea650de78eae4bdd8c7f928a08
Marcos Mathias <marcos-github@memathias.com>
2020-09-13 21:28:04 -0300

    Fix break words inside card text

--- frontend/src/components/CharacterCard/styles.ts
+++ frontend/src/components/CharacterCard/styles.ts
@@ -140,10 +140,7 @@ export const CharInfo = styled.div`
     text-decoration: none;
     transition: color 0.2s;
 
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
+    overflow-wrap: break-word;
 
     color: #000;
 
