74632176edfd26b36a98357b3b533f76c6917613
Marcos Mathias <marcos-github@memathias.com>
2021-01-16 16:27:53 -0300

    Working with canvas

--- frontend/src/components/CharKindredCard/index.tsx
+++ frontend/src/components/CharKindredCard/index.tsx
@@ -125,7 +125,7 @@ const CharKindredCard: React.FC<ICharacterCardProps> = ({
         <label htmlFor={charId}>
           <ProfileImage locked={locked}>
             {saving ? (
-              <img src={charImg} alt="" crossOrigin="anonymous" />
+              <img src={`${charImg}&t=${charId}`} alt="" />
             ) : (
               <img src={charImg} alt="" />
             )}
