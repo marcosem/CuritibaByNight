720c1a0fb6d998de6a42fff44005f8d37961dec9
Marcos Mathias <marcos-github@memathias.com>
2021-01-24 19:12:22 -0300

    Update Retainer XP calculation rate

--- backend/src/modules/characters/utils/calculateRetainerXP.ts
+++ backend/src/modules/characters/utils/calculateRetainerXP.ts
@@ -12,31 +12,35 @@ export default function calculateRetainerXP({
   const parsedRetainerLevel = Math.round(retainerLevel);
 
   switch (parsedRetainerLevel) {
-    case 2:
+    case 1:
       newXP = regnantXP * 0.05;
       break;
-    case 3:
-    case 20:
+    case 2:
+    case 10:
       newXP = regnantXP * 0.1;
       break;
-    case 4:
+    case 3:
       newXP = regnantXP * 0.15;
       break;
-    case 5:
-    case 30:
+    case 4:
+    case 20:
       newXP = regnantXP * 0.2;
       break;
-    case 6:
+    case 5:
       newXP = regnantXP * 0.25;
       break;
-    case 40:
+    case 6:
+    case 30:
       newXP = regnantXP * 0.3;
       break;
-    case 50:
+    case 40:
       newXP = regnantXP * 0.4;
       break;
-    case 60:
+    case 50:
       newXP = regnantXP * 0.5;
+      break;
+    case 60:
+      newXP = regnantXP * 0.6;
       break;
     default:
       newXP = 0;
