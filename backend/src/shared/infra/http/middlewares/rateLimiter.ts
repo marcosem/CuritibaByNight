78b48c342c32378f036752c114935dd43b57aaef
Marcos Mathias <marcos-github@memathias.com>
2021-01-24 20:08:41 -0300

    Update the rate limiter on backend

--- backend/src/shared/infra/http/middlewares/rateLimiter.ts
+++ backend/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -12,7 +12,7 @@ const redisClient = redis.createClient({
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: 'ratelimit',
-  points: 10,
+  points: 15,
   duration: 1,
 });
 
