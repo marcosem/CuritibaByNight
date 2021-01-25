34f41110518fcc2f72dd655f2897dc6c9d4d1d29
Marcos Mathias <marcos-github@memathias.com>
2021-01-25 19:17:06 -0300

    Fix scrollbar style

--- frontend/src/components/CharacterList/styles.ts
+++ frontend/src/components/CharacterList/styles.ts
@@ -25,18 +25,18 @@ export const Character = styled.div<ICharacterProps>`
 
       scrollbar-width: thin;
       scrollbar-color: #555;
-      scrollbar-track-color: #f5f5f5;
+      scrollbar-track-color: #333333; // #f5f5f5;
       scroll-behavior: smooth;
 
       &::-webkit-scrollbar {
         width: 8px;
-        background-color: #f5f5f5;
+        background-color: #333333; // #f5f5f5;
       }
 
       &::-webkit-scrollbar-track {
         box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
         border-radius: 8px;
-        background-color: #f5f5f5;
+        background-color: #333333; // #f5f5f5;
       }
 
       &::-webkit-scrollbar-thumb {
