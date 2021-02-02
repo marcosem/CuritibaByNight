d9de3ccd085f8c327b916a6f46b3c64f768fd843
Marcos Mathias <marcos-github@memathias.com>
2021-02-02 13:28:45 -0300

    Clean the code

--- frontend/src/utils/getSocket.ts
+++ frontend/src/utils/getSocket.ts
@@ -1,8 +1,6 @@
-// const socketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
 const socketUrl = process.env.REACT_APP_API_URL?.replace('https', 'ws')
   .replace('api', 'ws')
   .replace('http', 'ws');
-// .replace('3333', '3334');
 
 export default function getSocket(): WebSocket {
   const echoSocketUrl = `${socketUrl}/ws`;
