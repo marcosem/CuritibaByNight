2bef605058902d64e87e3da729517da18dfbfb27
Marcos Mathias <marcos-github@memathias.com>
2021-07-18 19:09:55 -0300

    feat: Creat Addons for Location Table

---
+++ backend/src/shared/infra/typeorm/migrations/1626636345875-CreateAddonsTable.ts
@@ -0,0 +1,126 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAddonsTable1626636345875
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'addons',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'level',
+            type: 'numeric',
+            default: 1,
+          },
+          {
+            name: 'description',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'defense',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'surveillance',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'req_background',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_merit',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_influence',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_other',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_addon_1',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_addon_2',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'req_addon_3',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'ability',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'ability_qty',
+            type: 'numeric',
+            default: 0,
+          },
+          {
+            name: 'influence',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'influence_qty',
+            type: 'numeric',
+            default: 0,
+          },
+          {
+            name: 'time_qty',
+            type: 'numeric',
+            default: 0,
+          },
+          {
+            name: 'time_type',
+            type: 'enum',
+            enum: ['days', 'weeks', 'months', 'years'],
+            default: "'weeks'",
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('addons');
+  }
+}
