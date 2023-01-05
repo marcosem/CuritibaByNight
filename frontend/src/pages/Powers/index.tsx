6a50d6f0a7c568bcd3a294a6235d486ad42cf151
Marcos Mathias <marcos-github@memathias.com>
2023-01-05 00:00:11 -0300

    feat: added endpoint connection to Powers page

--- frontend/src/pages/Powers/index.tsx
+++ frontend/src/pages/Powers/index.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable camelcase */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 // import { Link, useHistory } from 'react-router-dom';
-// import api from '../../services/api';
+
+import { FiEdit, FiTrash2 } from 'react-icons/fi';
+
+import Skeleton from '@material-ui/lab/Skeleton';
+import api from '../../services/api';
 
 // import Loading from '../../components/Loading';
 
@@ -9,24 +13,289 @@ import {
   Container,
   TitleBox,
   TableWrapper,
+  StyledTableContainer,
   StyledTable,
   StyledTableHead,
   StyledTableBody,
   StyledTableRow,
   StyledTableCell,
+  StyledTableSortLabel,
+  ActionsContainer,
+  ActionButton,
 } from './styles';
 
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import { useMobile } from '../../hooks/mobile';
 import { useHeader } from '../../hooks/header';
 
+interface IPowerSimple {
+  id: string;
+  name: string;
+  level: number;
+  type: string;
+  included: string;
+}
+
+interface IPowerResponse {
+  id: string;
+  long_name: string;
+  short_name: string;
+  level: number;
+  type: string;
+  origin?: string;
+  requirements?: string;
+  description?: string;
+  system?: string;
+  cost?: number;
+  source?: string;
+}
+
+interface ISort {
+  title: 'power' | 'level' | 'type' | 'included';
+  direction: 'asc' | 'desc';
+  active: boolean;
+}
+
 // using https://v4.mui.com/components
 const Powers: React.FC = () => {
+  const [powersList, setPowersList] = useState<IPowerSimple[]>([]);
+  const [sortOrder, setSortOrder] = useState<ISort[]>([
+    {
+      title: 'power',
+      direction: 'desc',
+      active: true,
+    },
+    {
+      title: 'level',
+      direction: 'asc',
+      active: false,
+    },
+    {
+      title: 'type',
+      direction: 'asc',
+      active: false,
+    },
+    {
+      title: 'included',
+      direction: 'asc',
+      active: false,
+    },
+  ]);
+  const [isBusy, setBusy] = useState(true);
+  const { signOut } = useAuth();
+  const { addToast } = useToast();
+
   const { isMobileVersion } = useMobile();
   const { setCurrentPage } = useHeader();
 
+  const translateType = useCallback(type => {
+    let traslatedType: string;
+
+    switch (type) {
+      case 'rituals':
+        traslatedType = 'Ritual';
+        break;
+      case 'powers':
+        traslatedType = 'Poder';
+        break;
+      case 'discipline':
+        traslatedType = 'Disciplina';
+        break;
+      case 'combination':
+        traslatedType = 'Combo';
+        break;
+      case 'gift':
+        traslatedType = 'Dom';
+        break;
+      case 'arcanoi':
+        traslatedType = 'Arcanoi';
+        break;
+      case 'spheres':
+        traslatedType = 'Esfera';
+        break;
+      case 'routes':
+        traslatedType = 'Rotina';
+        break;
+      case 'other':
+      default:
+        traslatedType = 'Outro';
+        break;
+    }
+
+    return traslatedType;
+  }, []);
+
+  const loadPowers = useCallback(async () => {
+    try {
+      await api.get('/powers/list').then(response => {
+        const res = response.data;
+
+        const levelsMap = ['Básico', 'Intermediário', 'Avançado'];
+
+        const newArray = res.map((power: IPowerResponse) => {
+          let level;
+
+          if (power.type === 'rituals') {
+            level = power.level <= 3 ? levelsMap[power.level - 1] : power.level;
+          } else if (power.level === 0) {
+            level = '-';
+          } else {
+            level = power.level;
+          }
+
+          const included = power.description ? 'Sim' : 'Não';
+
+          const newPower = {
+            id: power.id,
+            name: power.long_name,
+            level,
+            type: translateType(power.type),
+            included,
+          };
+
+          return newPower;
+        });
+
+        setPowersList(newArray);
+      });
+    } catch (error) {
+      const parsedError: any = error;
+
+      if (parsedError.response) {
+        const { message } = parsedError.response.data;
+
+        if (
+          message?.indexOf('token') > 0 &&
+          parsedError.response.status === 401
+        ) {
+          addToast({
+            type: 'error',
+            title: 'Sessão Expirada',
+            description: 'Sessão de usuário expirada, faça o login novamente!',
+          });
+
+          signOut();
+        } else {
+          addToast({
+            type: 'error',
+            title: 'Erro ao tentar listar os poderes',
+            description: `Erro: '${message}'`,
+          });
+        }
+      }
+    }
+    setBusy(false);
+  }, [addToast, signOut, translateType]);
+
+  const handleSort = useCallback(
+    title => {
+      let newSortItem: ISort = {
+        title,
+        direction: 'desc',
+        active: true,
+      };
+
+      const newSortOrder = sortOrder.map(sortElem => {
+        let newSortElem: ISort;
+
+        if (sortElem.title === title) {
+          newSortItem = {
+            title: sortElem.title,
+            direction: sortElem.direction === 'asc' ? 'desc' : 'asc',
+            active: true,
+          };
+          newSortElem = newSortItem;
+        } else {
+          newSortElem = {
+            title: sortElem.title,
+            direction: sortElem.direction,
+            active: false,
+          };
+        }
+
+        return newSortElem;
+      });
+
+      setSortOrder(newSortOrder);
+
+      // Sorting
+      const newPowersList = powersList;
+      switch (newSortItem.title) {
+        case 'level':
+          if (newSortItem.direction === 'asc') {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.level > powerB.level) return -1;
+              if (powerA.level < powerB.level) return 1;
+              return 0;
+            });
+          } else {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.level > powerB.level) return 1;
+              if (powerA.level < powerB.level) return -1;
+              return 0;
+            });
+          }
+          break;
+
+        case 'type':
+          if (newSortItem.direction === 'asc') {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.type > powerB.type) return -1;
+              if (powerA.type < powerB.type) return 1;
+              return 0;
+            });
+          } else {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.type > powerB.type) return 1;
+              if (powerA.type < powerB.type) return -1;
+              return 0;
+            });
+          }
+          break;
+
+        case 'included':
+          if (newSortItem.direction === 'asc') {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.included > powerB.included) return -1;
+              if (powerA.included < powerB.included) return 1;
+              return 0;
+            });
+          } else {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.included > powerB.included) return 1;
+              if (powerA.included < powerB.included) return -1;
+              return 0;
+            });
+          }
+          break;
+
+        case 'power':
+        default:
+          if (newSortItem.direction === 'asc') {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.name > powerB.name) return -1;
+              if (powerA.name < powerB.name) return 1;
+              return 0;
+            });
+          } else {
+            newPowersList.sort((powerA: IPowerSimple, powerB: IPowerSimple) => {
+              if (powerA.name > powerB.name) return 1;
+              if (powerA.name < powerB.name) return -1;
+              return 0;
+            });
+          }
+          break;
+      }
+      setPowersList(newPowersList);
+    },
+    [powersList, sortOrder],
+  );
+
   useEffect(() => {
     setCurrentPage('powers');
-  }, [setCurrentPage]);
+    loadPowers();
+  }, [loadPowers, setCurrentPage]);
 
   return (
     <Container isMobile={isMobileVersion}>
@@ -34,51 +303,96 @@ const Powers: React.FC = () => {
         <strong>Lista de Poderes</strong>
       </TitleBox>
       <TableWrapper>
-        <StyledTable>
-          <StyledTableHead>
-            <StyledTableRow>
-              <StyledTableCell>Poder</StyledTableCell>
-              <StyledTableCell>Nível</StyledTableCell>
-              <StyledTableCell>Tipo</StyledTableCell>
-              <StyledTableCell>Incluso?</StyledTableCell>
-              <StyledTableCell>Ações</StyledTableCell>
-            </StyledTableRow>
-          </StyledTableHead>
-          <StyledTableBody>
-            <StyledTableRow>
-              <StyledTableCell align="left">
-                Accord: Rite of Cleansing
-              </StyledTableCell>
-              <StyledTableCell align="center">Básico</StyledTableCell>
-              <StyledTableCell align="center">Ritual</StyledTableCell>
-              <StyledTableCell align="center">Sim</StyledTableCell>
-              <StyledTableCell align="center">x x x x</StyledTableCell>
-            </StyledTableRow>
-            <StyledTableRow>
-              <StyledTableCell align="left">
-                Ahroun: Falling Touch
-              </StyledTableCell>
-              <StyledTableCell align="center">1</StyledTableCell>
-              <StyledTableCell align="center">Poder</StyledTableCell>
-              <StyledTableCell align="center">Não</StyledTableCell>
-              <StyledTableCell align="center">x x x x</StyledTableCell>
-            </StyledTableRow>
-            <StyledTableRow>
-              <StyledTableCell align="left">Animalism</StyledTableCell>
-              <StyledTableCell align="center">1</StyledTableCell>
-              <StyledTableCell align="center">Disciplina</StyledTableCell>
-              <StyledTableCell align="center">Sim</StyledTableCell>
-              <StyledTableCell align="center">x x x x</StyledTableCell>
-            </StyledTableRow>
-            <StyledTableRow>
-              <StyledTableCell align="left">Animalism</StyledTableCell>
-              <StyledTableCell align="center">2</StyledTableCell>
-              <StyledTableCell align="center">Poder</StyledTableCell>
-              <StyledTableCell align="center">Não</StyledTableCell>
-              <StyledTableCell align="center">x x x x</StyledTableCell>
-            </StyledTableRow>
-          </StyledTableBody>
-        </StyledTable>
+        <StyledTableContainer>
+          <StyledTable stickyHeader>
+            <StyledTableHead>
+              <StyledTableRow>
+                <StyledTableCell onClick={() => handleSort(sortOrder[0].title)}>
+                  Poder
+                  <StyledTableSortLabel
+                    active={sortOrder[0].active}
+                    direction={sortOrder[0].direction}
+                  />
+                </StyledTableCell>
+                <StyledTableCell onClick={() => handleSort(sortOrder[1].title)}>
+                  Nível
+                  <StyledTableSortLabel
+                    active={sortOrder[1].active}
+                    direction={sortOrder[1].direction}
+                  />
+                </StyledTableCell>
+                <StyledTableCell onClick={() => handleSort(sortOrder[2].title)}>
+                  Tipo
+                  <StyledTableSortLabel
+                    active={sortOrder[2].active}
+                    direction={sortOrder[2].direction}
+                  />
+                </StyledTableCell>
+                <StyledTableCell onClick={() => handleSort(sortOrder[3].title)}>
+                  Incluso?
+                  <StyledTableSortLabel
+                    active={sortOrder[3].active}
+                    direction={sortOrder[3].direction}
+                  />
+                </StyledTableCell>
+                <StyledTableCell>Ações</StyledTableCell>
+              </StyledTableRow>
+            </StyledTableHead>
+            <StyledTableBody>
+              {isBusy ? (
+                <StyledTableRow>
+                  <StyledTableCell align="left">
+                    <Skeleton />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Skeleton />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Skeleton />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Skeleton />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Skeleton />
+                  </StyledTableCell>
+                </StyledTableRow>
+              ) : (
+                powersList.map(power => (
+                  <StyledTableRow key={`${power.name}-${power.level}`}>
+                    <StyledTableCell align="left">{power.name}</StyledTableCell>
+                    <StyledTableCell align="center">
+                      {power.level}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {power.type}
+                    </StyledTableCell>
+                    <StyledTableCell align="center" included={power.included}>
+                      {power.included}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      <ActionsContainer>
+                        <ActionButton
+                          id={`edit:${power.name}-${power.level}`}
+                          title="Editar"
+                        >
+                          <FiEdit />
+                        </ActionButton>
+                        <ActionButton
+                          id={`delete:${power.name}-${power.level}`}
+                          title="Remover"
+                          disabled={power.included === 'Não'}
+                        >
+                          <FiTrash2 />
+                        </ActionButton>
+                      </ActionsContainer>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))
+              )}
+            </StyledTableBody>
+          </StyledTable>
+        </StyledTableContainer>
       </TableWrapper>
     </Container>
   );
--- frontend/src/pages/Powers/styles.ts
+++ frontend/src/pages/Powers/styles.ts
@@ -1,14 +1,25 @@
 import styled, { css } from 'styled-components';
+import { shade } from 'polished';
 import {
+  TableContainer,
   Table,
   TableHead,
   TableBody,
   TableRow,
   TableCell,
+  TableSortLabel,
 } from '@material-ui/core';
 
 interface IContainerProps {
   isMobile: boolean;
+}
+
+interface ICellProps {
+  included?: string;
+}
+
+interface IButtonProps {
+  readonly type: 'button' | 'submit' | 'reset' | undefined;
 }
 
 export const Container = styled.div<IContainerProps>`
@@ -51,7 +62,37 @@ export const TableWrapper = styled.div`
   margin: 10px auto;
   min-width: 340px;
   max-width: 1012px;
-  // border-radius: 11px;
+`;
+
+export const StyledTableContainer = styled(TableContainer)`
+  && {
+    height: 75vh;
+
+    scrollbar-width: thin;
+    scrollbar-color: #555;
+    scrollbar-track-color: #f5f5f5;
+    scroll-behavior: smooth;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+      // background-color: #f5f5f5;
+      background-color: transparent;
+    }
+
+    &::-webkit-scrollbar-track {
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+      border-radius: 8px;
+      background-color: #f5f5f5;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 8px;
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+      background-color: #555;
+    }
+
+    border-radius: 10px;
+  }
 `;
 
 export const StyledTable = styled(Table)``;
@@ -61,79 +102,161 @@ export const StyledTableHead = styled(TableHead)``;
 export const StyledTableBody = styled(TableBody)``;
 
 export const StyledTableRow = styled(TableRow)`
-  ${StyledTableHead} & {
-  }
+  // Prioritize the CSS rules of styled-component over those of JSS
+  && {
+    ${StyledTableBody} & {
+      &:nth-of-type(odd) {
+        background-color: white;
+      }
 
-  ${StyledTableBody} & {
-    &:nth-of-type(odd) {
-      background-color: white;
-    }
+      &:nth-of-type(even) {
+        background-color: #e8e7e7;
+      }
 
-    &:nth-of-type(even) {
-      background-color: #e8e7e7;
-    }
+      &:hover {
+        background-color: #aaa;
+        color: #fff;
+      }
 
-    &:hover {
-      cursor: pointer;
-      background-color: #aaa;
-      color: #fff;
-    }
+      &:last-of-type {
+        td {
+          &:first-of-type {
+            border-radius: 0 0 10px 0;
+          }
 
-    &:last-of-type {
-      td {
-        &:first-of-type {
-          border-radius: 0 0 10px 0;
-        }
-
-        &:last-of-type {
-          border-radius: 0 0 0 10px;
+          &:last-of-type {
+            border-radius: 0 0 0 10px;
+          }
         }
       }
     }
   }
 `;
 
-export const StyledTableCell = styled(TableCell)`
-  ${StyledTableHead} & {
-    font-family: 'Roboto', sans-serif;
-    font-size: 12px;
-    font-weight: 450;
-    padding: 8px;
-    text-align: center;
-    line-height: 14px;
-
-    &:first-of-type {
-      border-radius: 10px 0 0 0;
-    }
-
-    &:last-of-type {
-      border-radius: 0 10px 0 0;
-    }
-
-    &:nth-of-type(odd) {
+export const StyledTableCell = styled(TableCell)<ICellProps>`
+  // Prioritize the CSS rules of styled-component over those of JSS
+  && {
+    ${StyledTableHead} & {
+      font-family: 'Roboto', sans-serif;
+      font-size: 12px;
+      font-weight: 450;
+      padding: 8px;
+      text-align: center;
+      line-height: 14px;
       color: #fff;
-      background: #0d0d0d;
+
+      &:first-of-type {
+        border-radius: 10px 0 0 0;
+      }
+
+      &:last-of-type {
+        border-radius: 0 10px 0 0;
+      }
+
+      &:nth-of-type(odd) {
+        background: #0d0d0d;
+      }
+
+      &:nth-of-type(even) {
+        background: #560209;
+      }
+
+      &:not(:last-of-type) {
+        &:hover {
+          cursor: pointer;
+        }
+      }
     }
 
-    &:nth-of-type(even) {
-      color: #fff;
-      background: #560209;
+    ${StyledTableBody} & {
+      font-family: 'Roboto', sans-serif;
+      font-size: 12px;
+      font-weight: normal;
+      padding: 8px;
+      line-height: 24px;
+
+      &:not(:first-of-type) {
+        border-left: 1px solid #ddd;
+      }
+
+      &:not(:last-of-type) {
+        border-right: 1px solid #ddd;
+      }
+
+      &:last-of-type {
+        width: 75px;
+      }
+
+      ${props =>
+        props.included === 'Sim' &&
+        css`
+          color: #028609;
+        `}
+
+      ${props =>
+        props.included === 'Não' &&
+        css`
+          color: #860209;
+        `}
+
+    ${props =>
+        props.included === undefined &&
+        css`
+          color: rgba(0, 0, 0, 0.87);
+        `}
     }
   }
+`;
 
-  ${StyledTableBody} & {
-    font-family: 'Roboto', sans-serif;
-    font-size: 12px;
-    font-weight: normal;
-    padding: 8px;
-    line-height: 14px;
-
-    &:not(:first-of-type) {
-      border-left: 1px solid #ddd;
-    }
-
-    &:not(:last-of-type) {
-      border-right: 1px solid #ddd;
+export const StyledTableSortLabel = styled(TableSortLabel)`
+  // Prioritize the CSS rules of styled-component over those of JSS
+  && {
+    svg {
+      color: #fff !important;
     }
   }
+`;
+
+export const ActionsContainer = styled.div`
+  display: flex;
+  flex-direction: space-between;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ActionButton = styled.button.attrs<IButtonProps>(() => ({
+  type: 'button',
+}))`
+  width: 24px;
+  height: 24px;
+
+  margin: auto;
+
+  display: flex;
+  flex-direction: space-between;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: 6px;
+  transition: background-color 0.2s;
+
+  background: #860209;
+  &:hover {
+    background: ${shade(0.2, '#860209')};
+  }
+
+  svg {
+    color: #fff;
+    width: 12px;
+    height: 12px;
+  }
+
+  ${props =>
+    props.disabled &&
+    css`
+      cursor: default;
+      opacity: 0.7;
+      // background: ${shade(0.3, '#860209')};
+    `}
 `;
